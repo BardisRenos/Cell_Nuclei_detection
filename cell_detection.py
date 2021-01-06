a61a9e6c1f8b8f71f98a69a72f8d332386ff0a63
Renos Bardis <renosbardis@gmail.com>
2021-01-06 10:26:56 +0100

    new addition

--- cell_detection.py
+++ cell_detection.py
@@ -1,6 +1,10 @@
+#! /usr/bin python3
+# -*- coding: utf-8 -*-
+
 import cv2
 import numpy as np
 from skimage import color
+from util import *
 
 
 def convert_to_gray_scale(image):
@@ -10,28 +14,41 @@ def convert_to_gray_scale(image):
 
 def image_preprocessing(given_image):
     image_gray_scale = convert_to_gray_scale(given_image)
-    pure_image = given_image
+    _, image_threshold_bw = cv2.threshold(image_gray_scale, 100, 255, cv2.THRESH_OTSU)
 
-    _, image_threshold_bw = cv2.threshold(image_gray_scale, 240, 255, cv2.THRESH_BINARY)
     kernel = np.ones((3, 3), np.uint8)
-    image_morph = cv2.morphologyEx(image_threshold_bw, cv2.MORPH_CLOSE, kernel, iterations=9)
+    image_morph = cv2.morphologyEx(image_threshold_bw, cv2.MORPH_CLOSE, kernel, iterations=3)
     image_mask = 255 - image_morph
 
-    background = cv2.dilate(image_mask, kernel, iterations=5)
+    background = cv2.dilate(image_mask, kernel, iterations=2)
+    show_2_images_with_matplot(image_threshold_bw, image_mask, "BW", "morph")
     dist_transform = cv2.distanceTransform(image_mask, cv2.DIST_L2, 3)
     _, foreground = cv2.threshold(dist_transform, 0.285 * dist_transform.max(), 255, 0)
     the_unknown_image = background - foreground
     foreground = np.uint8(foreground)
 
-    def markers_creation(image):
-        _, image_markers = cv2.connectedComponents(foreground, connectivity=8)
-        image_markers = image_markers + 10
-        image_markers[the_unknown_image == 255] = 0
+    # return foreground, the_unknown_image
 
-    def watershed():
-        image_markers = cv2.watershed(pure_image, image_markers)
-        pure_image[image_markers == -1] = [0, 255, 0]
-        image_label2rgb = color.label2rgb(image_markers, bg_label=0)
+    # def markers_creation():
+    #     # foreground, the_unknown_image = image_preprocessing(given_image)
+    #     _, image_markers = cv2.connectedComponents(foreground, connectivity=8)
+    #     image_markers = image_markers + 10
+    #     image_markers[the_unknown_image == 255] = 0
+    #
+    #     # return image_markers
+    #
+    # def watershed():
+    #     # image_markers = markers_creation()
+    #     image_markers = cv2.watershed(pure_image, image_markers)
+    #     pure_image[image_markers == -1] = [0, 255, 0]
+    #     image_label2rgb = color.label2rgb(image_markers, bg_label=0)
+
+    # return image_label2rgb
 
 
 if __name__ == '__main__':
+    image_path = '/home/renos/Pictures/sample_3.png'
+    image = read_image(image_path)
+
+    # show_image_with_matplot(image[:, :, 0])
+    image_preprocessing(image)
--- util.py
+++ util.py
@@ -2,14 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import cv2
-import numpy as np
 from matplotlib import pyplot as plt
+from skimage.segmentation import clear_border
 
 
+# Reading an image and returning the image
 def read_image(path):
     return cv2.imread(path)
 
 
+# Remove cells that are touching the edge of the image
+def clear_borders(image):
+    return clear_border(image)
+
+
+# Showing two images by using Matplotlib library
 def show_2_images_with_matplot(image1, image2, title1, title2):
     plt.figure(figsize=(20, 12))
     plt.subplot(121), plt.imshow(image1, cmap='gray'), plt.title(title1)
@@ -17,6 +24,7 @@ def show_2_images_with_matplot(image1, image2, title1, title2):
     plt.show()
 
 
+# Showing the image by using OpenCV
 def show_image_with_matplot(image1):
     plt.imshow(image1, cmap='gray'), plt.title("Given Image")
     plt.show()
